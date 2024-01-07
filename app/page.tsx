d7052585d27869a2cb8fdf655993cef287f45fe6
ZaneFerns360 <zane.ferns360@gmail.com>
2024-01-07 16:42:29 +0530

    improvements

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="w-full">
-      <NavBar />
-      <MultiLayerParallax />
+      <div className="bg-[url('/assets/bg.png')]">
+        {' '}
+        <NavBar />
+        <MultiLayerParallax />
+      </div>
+
       <div className="w-full">
         <About />
       </div>
