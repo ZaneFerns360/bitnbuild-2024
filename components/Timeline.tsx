745e8c67dffe99cf83c2d19ba060dcfc898ff78e
ZaneFerns360 <zane.ferns360@gmail.com>
2024-01-11 10:39:39 +0530

    timeline

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Timeline.css'
 
 const Timeline = () => (
-  <div className="flex h-full items-center justify-center bg-gray-800 bg-[url('/assets/stone.jpg')] bg-repeat">
+  <div className="flex h-full items-center justify-center bg-gray-800 bg-[url('/assets/lavaa.jpg')] bg-cover bg-bottom bg-no-repeat">
     <div className="wrap relative h-full overflow-hidden p-10">
       <div
         className="border-2-2 absolute h-full border border-gray-700 border-opacity-20"
@@ -14,7 +14,7 @@ const Timeline = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">1</h1>
         </div>
-        <div className="cards-right order-1 w-5/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
+        <div className="cards-right order-1 w-7/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">January 5th</h3>
           <p className="text-sm leading-snug tracking-wide text-white ">
             Registration for Internal State Hackathon.
@@ -27,7 +27,7 @@ const Timeline = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">2</h1>
         </div>
-        <div className="cards-left order-1 w-5/12 rounded-lg bg-red-400 px-6 py-4 shadow-xl">
+        <div className="cards-right order-1 w-7/12 rounded-lg bg-red-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">February 10th</h3>
           <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
             Hackathon Begins.
@@ -40,7 +40,7 @@ const Timeline = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">3</h1>
         </div>
-        <div className="cards-right order-1 w-5/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
+        <div className="cards-right order-1 w-7/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">February 10th</h3>
           <p className="text-sm leading-snug tracking-wide text-white text-opacity-100">
             Internal Hackathon Results.
--- components/Timeline2.tsx
+++ components/Timeline2.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Timeline.css'
 
 const Timeline2 = () => (
-  <div className="flex h-full items-center justify-center bg-gray-800 bg-[url('/assets/ob.gif')] bg-contain bg-repeat">
+  <div className="flex h-full items-center justify-center bg-gray-800 bg-[url('/assets/ob.gif')] bg-cover bg-repeat">
     <div className="wrap relative h-full overflow-hidden p-10">
       <div
         className="border-2-2 absolute h-full border border-gray-700 border-opacity-20"
@@ -14,7 +14,7 @@ const Timeline2 = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">1</h1>
         </div>
-        <div className="cards-right order-1 w-5/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
+        <div className="cards-left order-1 w-7/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">February 23rd</h3>
           <p className="text-sm leading-snug tracking-wide text-white ">
             Teams Arrive.
@@ -27,11 +27,11 @@ const Timeline2 = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">2</h1>
         </div>
-        <div className="cards-left order-1 w-5/12 rounded-lg bg-red-400 px-6 py-4 shadow-xl">
+        <div className="cards-left order-1 w-7/12 rounded-lg bg-red-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">
             February 23rd - February 24th
           </h3>
-          <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
+          <p className="text-sm font-medium leading-snug tracking-wide text-white">
             Grand Finale at Fr. Conceicao Rodrigues College of Engineering.
           </p>
         </div>
@@ -42,7 +42,7 @@ const Timeline2 = () => (
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">3</h1>
         </div>
-        <div className="cards-right order-1 w-5/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
+        <div className="cards-left order-1 w-7/12 rounded-lg bg-gray-400 px-6 py-4 shadow-xl">
           <h3 className="mb-3 text-xl font-bold text-white">February 24th</h3>
           <p className="text-sm leading-snug tracking-wide text-white text-opacity-100">
             Final Results of BitNBuild Hackathon.
