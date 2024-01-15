10144d191ac21ba742867ddd08a34fd7654620af
ZaneFerns360 <zane.ferns360@gmail.com>
2024-01-15 16:38:38 +0530

    added timeline animatiosn

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import React from 'react'
 import './Timeline.css'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
+import { fadeIn } from '@variants'
 
 const Timeline = () => (
   <div className="bg-[url('/assets/lavaa.jpg')] bg-cover bg-bottom bg-no-repeat">
@@ -17,7 +21,13 @@ const Timeline = () => (
     <div className="flex h-full items-center justify-center ">
       <div className="wrap relative h-full overflow-hidden p-10">
         {/* right timeline */}
-        <div className="right-timeline mb-8 flex w-full items-center justify-between">
+        <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: true, amount: 0.3 }}
+          className="right-timeline mb-8 flex w-full items-center justify-between"
+        >
           <div className="order-1 w-5/12"></div>
           <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
             <h1 className="mx-auto text-lg font-semibold text-white">1</h1>
@@ -30,9 +40,15 @@ const Timeline = () => (
               Registration for Internal State Hackathon.
             </p>
           </div>
-        </div>
+        </motion.div>
         {/* left timeline */}
-        <div className="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between">
+        <motion.div
+          variants={fadeIn('left', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: true, amount: 0.3 }}
+          className="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between"
+        >
           <div className="order-1 w-5/12"></div>
           <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
             <h1 className="mx-auto text-lg font-semibold text-white">2</h1>
@@ -43,9 +59,15 @@ const Timeline = () => (
               Hackathon Begins.
             </p>
           </div>
-        </div>
+        </motion.div>
         {/* right timeline */}
-        <div className="right-timeline mb-8 flex w-full items-center justify-between">
+        <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: true, amount: 0.3 }}
+          className="right-timeline mb-8 flex w-full items-center justify-between"
+        >
           <div className="order-1 w-5/12"></div>
           <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
             <h1 className="mx-auto text-lg font-semibold text-white">3</h1>
@@ -56,7 +78,7 @@ const Timeline = () => (
               Internal Hackathon Results.
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   </div>
--- components/Timeline2.tsx
+++ components/Timeline2.tsx
@@ -1,11 +1,21 @@
+'use client'
+
 import React from 'react'
 import './Timeline.css'
+import { motion } from 'framer-motion'
+import { fadeIn } from '@variants'
 
 const Timeline2 = () => (
   <div className="flex h-full items-center justify-center bg-gray-800 bg-[url('/assets/ob.gif')] bg-contain bg-repeat">
     <div className="wrap relative h-full overflow-hidden p-10">
       {/* right timeline */}
-      <div className="right-timeline mb-8 flex w-full items-center justify-between">
+      <motion.div
+        variants={fadeIn('right', 0.3)}
+        initial="hidden"
+        whileInView={'show'}
+        viewport={{ once: true, amount: 0.3 }}
+        className="right-timeline mb-8 flex w-full items-center justify-between"
+      >
         <div className="order-1 w-5/12"></div>
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">1</h1>
@@ -16,9 +26,15 @@ const Timeline2 = () => (
             Teams Arrive.
           </p>
         </div>
-      </div>
+      </motion.div>
       {/* left timeline */}
-      <div className="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between">
+      <motion.div
+        variants={fadeIn('left', 0.3)}
+        initial="hidden"
+        whileInView={'show'}
+        viewport={{ once: true, amount: 0.3 }}
+        className="left-timeline mb-8 flex w-full flex-row-reverse items-center justify-between"
+      >
         <div className="order-1 w-5/12"></div>
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">2</h1>
@@ -31,9 +47,15 @@ const Timeline2 = () => (
             Grand Finale at Fr. Conceicao Rodrigues College of Engineering.
           </p>
         </div>
-      </div>
+      </motion.div>
       {/* right timeline */}
-      <div className="right-timeline mb-8 flex w-full items-center justify-between">
+      <motion.div
+        variants={fadeIn('right', 0.3)}
+        initial="hidden"
+        whileInView={'show'}
+        viewport={{ once: true, amount: 0.3 }}
+        className="right-timeline mb-8 flex w-full items-center justify-between"
+      >
         <div className="order-1 w-5/12"></div>
         <div className="z-20 order-1 flex h-8 w-8 items-center rounded-full bg-gray-800 shadow-xl">
           <h1 className="mx-auto text-lg font-semibold text-white">3</h1>
@@ -44,7 +66,7 @@ const Timeline2 = () => (
             Final Results of BitNBuild Hackathon.
           </p>
         </div>
-      </div>
+      </motion.div>
     </div>
   </div>
 )
