467a8d72274ce50f2957cf1ae64f54bd136ca23f
arcosane <castelinoaston123@gmail.com>
2024-01-22 01:17:09 +0530

    Prizes-2

--- components/Prizes.tsx
+++ components/Prizes.tsx
@@ -28,7 +28,7 @@ export const Prizes = () => {
       </div>
 
       {/* Static Grid of Prize1 Images */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-0 px-4 text-center md:mt-0">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap- px-4 text-center md:mt-0">
         <div>
           <Image
             className="mx-auto hidden md:inline opacity-95"
@@ -39,20 +39,20 @@ export const Prizes = () => {
             style={{opacity:0.3}}
           />
         </div>
-        <div className='grid md:grid-cols-3 grid-cols-1 gap-4 justify-center items-center'>
-          <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600">
-          <HiMiniTrophy className='text-slate-300 mx-auto my-4 text-5xl'/>
-            <h2 className="text-2xl text-white font-bold">2nd Place</h2>
-            <p className="text-white">₹25,000</p>
-          </div>
-          <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600">
+        <div className='grid md:grid-cols-3 grid-cols-1 gap-4 justify-center items-center md:my-0 my-5'>
+        <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600 md:order-2 ">
             <GiTrophy className='text-amber-500 mx-auto my-4 text-5xl'/>
-            <h2 className="text-2xl text-white font-bold">1st Place</h2>
+            <h2 className="text-2xl text-white font-bold">I Place</h2>
             <p className="text-white">₹50,000</p>
           </div>
-          <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600">
+          <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600 md:order-1 md:mt-2">
+          <HiMiniTrophy className='text-slate-300 mx-auto my-4 text-5xl'/>
+            <h2 className="text-2xl text-white font-bold">II Place</h2>
+            <p className="text-white">₹25,000</p>
+          </div>
+          <div className="bg-slate-700 p-10 rounded-lg shadow-md hover:bg-slate-600 md:order-3 md:mt-2">
             <GiTrophyCup className='text-amber-700 mx-auto my-4 text-5xl'/>
-            <h2 className="text-2xl text-white font-bold">3rd place</h2>
+            <h2 className="text-2xl text-white font-bold">III Place</h2>
             <p className="text-white">₹10,000</p>
           </div>
         </div>
